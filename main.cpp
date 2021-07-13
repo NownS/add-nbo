3881b50591566815e6bb7430b29e85f516d3db87
NownS <sksmsx147@gmail.com>
2021-07-13 05:42:52 -0700

    [ADD] add-nbo source

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <netinet/in.h>
+#include <cstdio>
+
+int main(int argc, char *argv[]){
+
+    if(argc != 3){
+        printf("syntax :  add-nbo <file1> <file2>\n");
+        printf("sample :  add-nbo a.bin b.bin\n");
+        return -1;
+    }
+
+    FILE * file1 = fopen(argv[1], "rb");
+    FILE * file2 = fopen(argv[2], "rb");
+
+    uint32_t f1, f2;
+
+    fread(&f1, sizeof(uint32_t), 1, file1);
+    fread(&f2, sizeof(uint32_t), 1, file2);
+
+    f1 = htonl(f1);
+    f2 = htonl(f2);
+
+    uint32_t result = f1 + f2;
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", f1, f1, f2, f2, result, result);
+
+    fclose(file1);
+    fclose(file2);
+}
