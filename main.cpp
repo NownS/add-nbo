505895cc7aee4665a09836e2bc7285a1eef59af5
NownS <kimjuicy29@gmail.com>
2021-07-15 23:15:15 -0700

    simplify main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <stdint.h>
 #include <netinet/in.h>
 #include <cstdio>
+#include <stdlib.h>
+
+uint32_t readint(char* filename){
+    FILE * file = fopen(filename, "rb");
+    uint32_t f;
+    size_t readlen = fread(&f, 1, sizeof(uint32_t), file);
+    if(readlen != sizeof(uint32_t)){
+        fprintf(stderr, "fread return %lu\n",readlen);
+        exit(-1);
+    }
+
+    f = ntohl(f);
+    fclose(file);
+    return f;
+}
+
 
 int main(int argc, char *argv[]){
 
@@ -10,20 +26,11 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    FILE * file1 = fopen(argv[1], "rb");
-    FILE * file2 = fopen(argv[2], "rb");
-
     uint32_t f1, f2;
-
-    fread(&f1, sizeof(uint32_t), 1, file1);
-    fread(&f2, sizeof(uint32_t), 1, file2);
-
-    f1 = ntohl(f1);
-    f2 = ntohl(f2);
+    f1 = readint(argv[1]);
+    f2 = readint(argv[2]);
 
     uint32_t result = f1 + f2;
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", f1, f1, f2, f2, result, result);
 
-    fclose(file1);
-    fclose(file2);
 }
